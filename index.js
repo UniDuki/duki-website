54668a29fbae3630c9af37784a91506e58636640
UniDuki <jduckyyy@gmail.com>
2021-02-27 09:14:26 +0000

    rainbow button

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let sidebar, bars;
 let sidebarCooldown = false;
 
+let hue = 1;
+let rainbow = false;
+let rainbowLoop;
+
+
 // Init function
 function init() {
     sidebar = document.getElementById("sidebar");
@@ -37,4 +42,25 @@ function toggleMenu() {
 
     sidebarCooldown = true;
     setTimeout(() => sidebarCooldown = false, 500);
+}
+
+function toggleRainbow() {
+    // Toggle
+    rainbow = rainbow ? false : true;
+
+    if (rainbow) {
+        rainbowLoop = setInterval(() => {
+            hue += 1;
+            if (hue > 360) hue = 1;
+
+            // document.body.style.backgroundColor = `hsl(${hue}, 100%, 65%)`;
+            document.body.style.backgroundImage =
+            `linear-gradient(45deg, hsl(${hue - 45}, 100%, 65%), hsl(${hue}, 100%, 65%))`;
+        }, 50);
+    }
+    else {
+        document.body.style.backgroundColor = "#f8f8f8";
+        document.body.style.backgroundImage = null;
+        clearInterval(rainbowLoop);
+    }
 }
