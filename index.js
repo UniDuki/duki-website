6951ff25db4f3d008328c4f265fac9ceb3ccd984
UniDuki <jduckyyy@gmail.com>
2021-03-03 13:09:20 +0000

    navbar maybe

--- index.js
+++ index.js
@@ -1,61 +1,16 @@
-let sidebar, bars;
-let sidebarCooldown = false;
+// Init function
+function init() {
+    // . . .
+}
 
-// Rainbow things
+
+// Add navbar to all
+
+
+// Rainbow
 let hue = 1;
 let rainbow = false;
 let rainbowLoop;
-
-
-let categories = [
-    { title: "Games", pages: [
-        { title: "Bounce", name: "bounce" },
-        { title: "Minesweeper", name: "minesweeper" },
-    ] },
-
-    { title: "Tools", pages: [
-        { title: "Combinations", name: "comb-calc" },
-    ] },
-];
-
-// Init function
-function init() {
-    sidebar = document.getElementById("sidebar");
-    bars = document.getElementById("bars");
-
-    // Pages
-    categories = categories.map((ctg) => {
-        return `<p class="row title">${ctg.title}</p>\n` + ctg.pages.map((page) => {
-            return `<a href="/pages/${page.name}" class="row">${page.title}</a>`;
-        }).join("\n");
-    });
-    document.getElementById("sidebar").innerHTML = categories.join("\n");
-
-    window.addEventListener("click", (event) => {
-        if (
-            !["bars", "sidebar", "bar1", "bar2", "bar3"].includes(event.target.id)
-            && sidebar.style.display === "block"
-            && sidebarCooldown !== true
-        ) {
-            sidebar.style.display = "none";
-            bars.classList.remove("change");
-        }
-    });
-
-    const t = document.getElementById("table");
-    new Util.Table(5, 5, t);
-}
-
-
-function toggleMenu() {
-    sidebar.style.display = sidebar.style.display === "block" ? "none" : "block";
-
-    bars.classList.toggle("change");
-
-    sidebarCooldown = true;
-    setTimeout(() => sidebarCooldown = false, 500);
-}
-
 
 function toggleRainbow() {
     // Toggle
